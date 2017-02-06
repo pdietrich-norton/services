a7e61242212b0fbf4f2ff5a61dd96c3e1ba85748
Dietrich, Peter <pdietrich@wwnorton.com>
2017-02-06 16:40:33 -0500

    Add redis caching and logging

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var cors = require("cors");
 var bodyParser = require("body-parser");
 var config = require('./lib/config');
 var searchController = require('./controllers/search');
+var log = require('./log')(module);
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -15,7 +17,22 @@ app.post('/search', searchController.getSearchData("search"));
 app.post('/search/morerefs', searchController.getSearchData("morerefs"));
 
 
-app.listen(config.get('port'));
+app.listen(config.get('port'), function() {
+    log.info('Express server listening on port ' + config.get('port'));
+});
+
+app.use(function(req, res, next){
+    res.status(404);
+    log.debug('Not found URL: %s',req.url);
+    res.send({ error: 'Not found' });
+    return;
+});
+
+app.use(function(err, req, res, next){
+    res.status(err.status || 500);
+    log.error('Internal error(%d): %s',res.statusCode,err.message);
+    res.send({ error: err.message });
+    return;
+});
 
 module.exports = app;
-
--- controllers/search.js
+++ controllers/search.js
@@ -1,23 +1,29 @@
 var config = require('../lib/config');
+var log = require('../log')(module);
 var rp = require('request-promise');
+var redis = require('../lib/redisCache');
 
 // Search from searchandiser
 exports.getSearchData = function(mode) {
     return function(req, res) {
         var uri,
-            query;
+            query,
+            redisKey;
+
         var searchParams = getSearchParams(req.body);
         if (searchParams.hasOwnProperty('error')) {
+            log.info('User error: %s', searchParams.error);
             return res.status(400).send({result: searchParams.error});
         }
 
         // Refinements query requires special request params
         if (mode === "morerefs") {
             if (!searchParams.hasOwnProperty("navigationName")) {
+                log.info('User error: Request parameters missing.');
                 return res.status(400).send({result: "Request parameters missing."});
             }
 
-            uri = config.get('default:search:refinementsUri');
+            uri = config.get('search:refinementsUri');
             query = {
                 navigationName: searchParams.navigationName,
                 originalQuery: {
@@ -31,39 +37,51 @@ exports.getSearchData = function(mode) {
                 query.originalQuery.query = searchParams.query;
             }
         } else {    // normal search query
-            uri = config.get('default:search:searchUri');
+            uri = config.get('search:searchUri');
             query = searchParams;
         }
 
-        rp({
-            method: "POST",
-            uri: uri,
-            body: query,
-            json: true
-        })
-            .then((data) => {
-                return res.send(data);
-            })
-            .catch((err) => {
-                return res.send(err);
-            })
-    };
+        redisKey = 'book_site-search-' + JSON.stringify(query);
+
+        redis.get(redisKey, function(error, result) {
+            if (result) {
+                return res.send(result);
+            } else {
+                rp({
+                    method: "POST",
+                    uri: uri,
+                    body: query,
+                    json: true
+                })
+                    .then((data) => {
+                        console.log(redisKey);console.log(data);
+                        redis.setex(redisKey, config.get('redis:searchExpiry'), JSON.stringify(data));
+                        return res.send(data);
+                    })
+                    .catch((err) => {
+                        return res.send(err);
+                    })
+
+            }
+
+        });
+    }
 };
 
 var getSearchParams = function(data) {
     var sortParam = {};
-    var configParams = config.get('default:search:configurableParams');
+    var configParams = config.get('search:configurableParams');
 
     var defaultParams = {
-        clientKey:  config.get('default:search:clientKey'),
-        collection: config.get('default:search:collection'),
-        area: config.get('default:search:area'),
-        fields: config.get('default:search:fields'),
-        pruneRefinements: config.get('default:search:pruneRefinements'),
-        skip: config.get('default:search:skip'),
-        pageSize: config.get('default:search:pageSize'),
-        disableAutocorrection: config.get('default:search:disableAutocorrection'),
-        matchStrategyName: config.get('default:search:matchStrategyName')
+        clientKey:  config.get('search:clientKey'),
+        collection: config.get('search:collection'),
+        area: config.get('search:area'),
+        fields: config.get('search:fields'),
+        pruneRefinements: config.get('search:pruneRefinements'),
+        skip: config.get('search:skip'),
+        pageSize: config.get('search:pageSize'),
+        disableAutocorrection: config.get('search:disableAutocorrection'),
+        matchStrategyName: config.get('search:matchStrategyName')
     };
 
     // Replace default values with any sent from request
